aca90df90f788cea27682549bd0b21b0b2b02f2f
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2023-09-27 20:59:27 -0400

    almost finished, but cant load foursquar

--- index.js
+++ index.js
@@ -1,67 +1,60 @@
-function addMarkers() {
-  for (var i = 0; i < this.business.length; i++) {
-    this.markers = L.marker([this.business[i].lat, this.business[i].long])
-      .bindPopup(`<p1>${this.business[i].name}</p1>`)
-      .addTo(this.map);
-  }
-}
+const myMap = {
+  coordinates: [],
+  businesses: [],
+  map: {},
+  markers: [],
 
-//Create Map
-async function main() {
-  let coordinates = await getCoords;
-  var map = L.map("map").setView([32.87031, -80.00015], 15);
-  const myMap = {
-    coordinates: [],
-    business: [],
-    map: {},
-    markers: {},
-  };
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
-  var marker = L.marker([32.87031, -80.00015]).addTo(map);
-  marker.bindPopup("<b>Hello! You Are Here!</b>").openPopup();
-
-  //submit button
-  document.getElementById("submit").addEventListener("click", async (event) => {
-    event.preventDefault();
-    let business = document.getElementById("business").value;
-    let data = await getFoursquare(business);
-    myMAp.business = processBusiness(data);
-    myMap.addMarkers();
-  });
-
-  async function getCoords() {
-    const pos = await new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
+  buildMap() {
+    this.map = L.map("map", {
+      center: this.coordinates,
+      zoom: 13,
     });
-    return [pos.coords.latitude, pos.coords.longitude];
-  }
-}
-main();
 
-//eventlistener
-window.onload = async () => {
-  const coords = await getCoords();
-  myMap.coordinates = coords;
-  myMap.buildMap();
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      minZoom: 8,
+    }).addTo(this.map);
+
+    const marker = L.marker(this.coordinates);
+    marker
+      .addTo(this.map)
+      .bindPopup("<p1><b>Hello, You are right here!</b><br></p1>")
+      .openPopup();
+  },
+  // add business markers
+  addMarkers() {
+    this.markers = [];
+    for (var i = 0; i < this.businesses.length; i++) {
+      this.markers = L.marker([this.businesses[i].lat, this.businesses[i].long])
+        .bindPopup(`<p1>${this.businesses[i].name}</p1>`)
+        .addTo(this.map);
+      //push each marker to the markers array
+      this.markers.push(marker);
+    }
+  },
 };
-
-async function getFoursquare(coordinates, business) {
+// get coordinates via geolocation api
+async function getCoords() {
+  const pos = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+  return [Math.floor(pos.coords.latitude), Math.floor(pos.coords.longitude)];
+}
+// get foursquare businesses
+async function getFoursquare(business) {
   const options = {
     method: "GET",
     headers: {
       Accept: "application/json",
-      Authorization: "fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8=",
+      Authorization: "fsq39nJkgK5siDaOrBajgtDkHuABZwU9Z2u2y5FfidMDeEQ=",
     },
   };
   let limit = 5;
   let lat = myMap.coordinates[0];
   let lon = myMap.coordinates[1];
   let response = await fetch(
-    `https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`,
+    `https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`,
     options
   );
   let data = await response.text();
@@ -74,10 +67,24 @@ function processBusinesses(data) {
   let businesses = data.map((element) => {
     let location = {
       name: element.name,
-      lat: element.geocodes.main.latitude,
-      long: element.geocodes.main.longitude,
+      lat: element.location.lat,
+      long: element.location.lng,
     };
     return location;
   });
   return businesses;
 }
+// window load
+window.onload = async () => {
+  const coords = await getCoords();
+  myMap.coordinates = coords;
+  myMap.buildMap();
+};
+// business submit button
+document.getElementById("submit").addEventListener("click", async (event) => {
+  event.preventDefault();
+  let business = document.getElementById("business").value;
+  let data = await getFoursquare(business);
+  myMap.businesses = processBusinesses(data);
+  myMap.addMarkers();
+});
